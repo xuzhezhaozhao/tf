38cc89379d419703b37e0876165c502820d186c9
zhezhaoxu <zhezhaoxu@tencent.com>
2017-12-02 18:09:02 +0800

    add expert.py

--- beginner.py
+++ beginner.py
@@ -8,24 +8,26 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(
+    -tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
 print("init W and b:\n")
-# sess.run(W)
-# sess.run(b)
+sess.run(W)
+sess.run(b)
 
 for _ in range(1000):
-  batch_xs, batch_ys = mnist.train.next_batch(100)
-  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 print("trained W and b: \n")
-# sess.run(W)
-# sess.run(b)
+sess.run(W)
+sess.run(b)
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print(
+    sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
