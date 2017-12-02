b9e5faae0fb92f0c3dd8312356dd5292503573e9
zhezhao <zhezhaoxu@gmail.com>
2017-12-02 09:55:39 +0800

    init

---
+++ reduce_sum.py
@@ -0,0 +1,6 @@
+x = tf.constant([[1, 1, 1], [1, 1, 1]])
+tf.reduce_sum(x)  # 6
+tf.reduce_sum(x, 0)  # [2, 2, 2]
+tf.reduce_sum(x, 1)  # [3, 3]
+tf.reduce_sum(x, 1, keep_dims=True)  # [[3], [3]]
+tf.reduce_sum(x, [0, 1])  # 6
